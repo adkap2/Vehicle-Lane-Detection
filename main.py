81c912dd45be1c8f3c0054641d1701d198f8ad44
Adam <adkap2@gmail.com>
2021-04-23 10:07:58 -0700

    updated cnn

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Softmax, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 import pickle
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from keras.preprocessing.image import ImageDataGenerator
+
 
 # here definition of constants, etc.
 #IMAGE_SHAPE = (160, 576)
@@ -34,9 +36,9 @@ def build_model(input_shape):
     """ Baseline model"""
     model = Sequential()
     model.add(BatchNormalization(input_shape=input_shape))
-    #model.add(Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
-    # Below layers were re-named for easier reading of model summary; this not necessary
-    # Conv Layer 1
+    # #model.add(Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
+    # # Below layers were re-named for easier reading of model summary; this not necessary
+    # # Conv Layer 1
     model.add(Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'))
 
     # Conv Layer 2
@@ -46,26 +48,26 @@ def build_model(input_shape):
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     # Conv Layer 3
-    model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'))
+    # model.add(Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'))
     model.add(Dropout(0.2))
 
     # Conv Layer 4
-    model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv4'))
+    model.add(Conv2D(256, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv4'))
     model.add(Dropout(0.2))
 
     # Conv Layer 5
-    model.add(Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv5'))
+    model.add(Conv2D(512, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv5'))
     model.add(Dropout(0.2))
 
     # Pooling 2
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     # Conv Layer 6
-    model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv6'))
+    model.add(Conv2D(512, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv6'))
     model.add(Dropout(0.2))
 
     # Conv Layer 7
-    model.add(Conv2D(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv7'))
+    model.add(Conv2D(1024, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv7'))
     model.add(Dropout(0.2))
 
     # Pooling 3
@@ -111,23 +113,6 @@ def build_model(input_shape):
 
     return model
 
-# def optimize(model, label, learning_rate, num_classes):
-
-#     with tf.name_scope("logits"):
-#         logits_ = tf.reshape(model, (-1, num_classes))
-#     labels_ = tf.reshaoe(label, (-1, num_classes))
-#     with tf.name_scope("softmax"):
-#         soft_max = tf.nn.softmax_cross_entropy_with_logits(logits=logits_, labels=labels_)
-#     with tf.name_scope("xent"):
-#         cross_entropy_loss = tf.reduce_mean(soft_max)
-#     with tf.name_scope("train"):
-#         optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate).minimize(cross_entropy_loss)
-
-#     tf.summary.histogram("logits", logits_)
-#     tf.summary.histogram("softmax", soft_max)
-#     tf.summary.scalar("xent", cross_entropy_loss)
-    
-#     return (logits_, optimizer, cross_entropy_loss)
 
 def fit_model(imgs, labels, model):
 
@@ -138,7 +123,6 @@ def fit_model(imgs, labels, model):
     train_images, labels = shuffle(train_images, labels)
 
     X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_size=0.1)
-    
 
     batch_size = 128
     epochs = 10
@@ -146,39 +130,16 @@ def fit_model(imgs, labels, model):
     datagen = ImageDataGenerator(channel_shift_range=0.2)
     datagen.fit(X_train)
 
-
+    callbacks = [EarlyStopping(monitor='loss', patience=3), ModelCheckpoint(filepath = 'model4.h5', monitor='val_loss', save_best_only=True)]
 
     history = model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size), steps_per_epoch=len(X_train)/batch_size,
-        epochs=epochs, verbose=1, validation_data=(X_val, y_val))
+        epochs=epochs, verbose=1, callbacks=callbacks, validation_data=(X_val, y_val))
 
-    model.save('full_CNN_model.h5')
+    model.save('model3.h5')
     model.summary()
     print(model.evaluate(X_train, y_train))
 
     return history
-
-
-# def run():
-#     data_dir = './data'
-#     runs_dir = './runs'
-#     save_file = './save/fcn_model_ckpt_' + time.strftime("%Y%m%d_%H%M%S")
-#     tests.test_for_kitti_dataset(data_dir)
-
-#     # helper.generate_augmented_images(os.path.join(data_dir, 'data_road/training'), IMAGE_SHAPE)
-
-#     # get_batches_fn = helper.get_batch_function(os.path.join(data_dir, 'data_road/training'), IMAGE_SHAPE)
-
-#     #correct_label = tf.Variable(tf.float32, shape = [None, None, None, NUM_CLASSES])
-#     #learning_rate = tf.Variable(tf.float32)
-
-#     model = build_model()
-#     print(model.summary())
-
-#     #X_train, y_train = helper.get_batch_function(os.path.join(data_dir, 'data_road/training'), IMAGE_SHAPE)
-
-#     #history = model.fit(X_train, y_train)
-#     model.fit_generator(generator=helper.get_batch_function(os.path.join(data_dir, 'data_road/training'), IMAGE_SHAPE))
-#     #logits, train_op, cross_entropy_loss = optimize(model, correct_label, learning_rate, NUM_CLASSES)
 
 
 def main():

e740b59289a88170849ad872a99c6c89deff7fd5
Adam <adkap2@gmail.com>
2021-04-23 11:22:01 -0700

    Updated predict lines file, added functional decomp

--- predict_lines.py
+++ predict_lines.py
@@ -13,7 +13,6 @@ class Lanes():
         self.recent_fit = []
         self.avg_fit = []
 
-
 def make_lines(image, model, lanes):
     """ Generates predicted lane segment based on trained model model
     Takes original image, passes it into model to predict lanes, then overlays
@@ -21,14 +20,13 @@ def make_lines(image, model, lanes):
     returns combined image"""
 
     # Get image ready for feeding into model
-    #print(image.shape)
-    # small_img = cv2.resize(image, (80, 160), 3)
-    small_img = image # Test
+
     small_img = np.array(small_img)
     small_img = small_img[None,:,:,:]
 
+    # OpenCV takes images in 0-255 format while NeuralNets prefer 0-1 format
     prediction = model.predict(small_img)[0] * 255
-    print(prediction)
+    # print(prediction)
 
     lanes.recent_fit.append(prediction)
     # Only using last five for average
@@ -43,39 +41,17 @@ def make_lines(image, model, lanes):
 
     # Re-size to match the original image
     lane_image = cv2.resize(lane_drawn, (160, 80), 3)
-    # lane_image = cv2.resize(lane_drawn, (720, 1280), 3)
+
+    # Plot two images side by side
+    plot_side_by_side(image, lane_image)
 
     # Merge the lane drawing onto the original image
+    combined = (image*1 + lane_image*1).astype(int)
 
-    #print(image.shape)
-    #print(lane_image.shape)
-    #print(type(image))
-    #print(type(lane_image))
-
-    #img1[image[:, :, 1:].all(axis=-1)] = 0
-    #img2[lane_image[:, :, 1:].all(axis=-1)] = 0
-
-    # dst = cv2.addWeighted(img1, 1, img2, 1, 0)
-
-    fig, axs = plt.subplots(1,2)
-    axs[0].imshow(lane_image)
-    axs[1].imshow(image)
-    # axs[2].imshow(dst)
-    plt.show()
-
-    # combined = (image*1 + lane_image*1).astype(int)
-    #print((image.shape))
-    #print((lane_image.shape))
-    #result = cv2.addWeighted(image, 0.7, lane_image, 0.3, 0)
-    #result = image + lane_image
-    #print(combined)
-    #plt.imshow(combined)
-    #plt.show()
-
-    return lane_image # SHould be combined
+    return combined # Should be combined
 
 def make_video():
-    """ Takes images from image directory and generates 
+    """ Takes images from image directory and generates
     video of overlayed images """
     img_array = []
     for filename in glob.glob('unseen_images/*.png'):
@@ -90,9 +66,19 @@ def make_video():
         out.write(img_array[i])
     out.release()
 
-def plotter(result):
+def plot_combined(result):
     """ Plots image overlayed image"""
+
     plt.imshow(result)
+    plt.show()
+
+def plot_side_by_side(image, lane_image):
+    """ Plots predicted and original image in side by side
+    format"""
+    fig, axs = plt.subplots(1,2)
+    axs[0].imshow(lane_image)
+    axs[1].imshow(image)
+
     plt.show()
 
 def load_images(path):
@@ -108,48 +94,52 @@ def load_images(path):
         i += 1
     return image_list
 
-def main():
-    #model = load_model('full_CNN_model.h5')
-    #model = load_model('baseline_cnn.h5')
-    model = load_model('model3.h5')
+def predict_unseen(path, model, lanes):
+    """Generates predictions from unseen image data
+    Currently has poor performance
+    """
+    imgs = load_images(path)
 
-    lanes = Lanes()
+    for i, img in enumerate(imgs[10:70]):
+        combined = make_lines(img, model, lanes)
+
+        im = Image.fromarray((combined).astype(np.uint8))
+        im.save(f"unseen_images/img{i}.png")
+
+def make_pred_seen(model, lanes):
+    """ makes prediction on already seen images """
+    #img = cv2.imread(path)
+    # load_image(path)
 
     pkl = open('full_CNN_train.p', 'rb')
     imgs = pickle.load(pkl)
+    combined0 = make_lines(imgs[400], model, lanes)
+    combined1 = make_lines(imgs[12], model, lanes)
+    plot_combined(combined0)
+    plot_combined(combined1)
 
-    path = "/Users/adam/Desktop/galvanize/\
-    capstones/semantic_lane_detect/data/data_road/testing/image_2/*.png"
-    #img = cv2.imread(path)
-    # load_image(path)
-    #img = cv2.resize(img, (160,80), 3)
+def get_model():
+    """ Returns selected CNN model to generate predictions on"""
 
-    #result = make_lines(img, model, lanes)
+    model = load_model('full_CNN_model.h5')
+    # model = load_model('baseline_cnn.h5')
+    # model = load_model('model3.h5')
 
-    #plotter(result)
+    return model
 
-    #road_lines = []
-    #print(model.evaluate())
+def main():
+    """ Main functions which calls all other functions in program file
+    """
+    model = get_model()
 
-    #imgs = load_images(path)
-    #make_lines(imgs[30], model, lanes)
-    make_lines(imgs[3], model, lanes)
+    lanes = Lanes()
+    make_pred_seen(model, lanes)
 
-    # for i, img in enumerate(imgs[0:70]):
-    #     combined = make_lines(img, model, lanes)
-    #     print(type(combined))
+    path = "/Users/adam/Desktop/galvanize/capstones/semantic_lane_detect/data/data_road/testing/image_2/*.png"
 
-    #     print((combined.shape))
-    #     im = Image.fromarray((combined).astype(np.uint8))
-    #     im.save(f"unseen_images/img{i}.png")
-
-    make_video()
-
-    #print(result)
-    #plt.imshow(result)
-    #plt.show()
+    # predict_unseen(path, model, lanes)
+    # make_video()
 
 
 if __name__=="__main__":
-
     main()

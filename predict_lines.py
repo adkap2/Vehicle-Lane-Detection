24e1a13014ffa2107087f19c3e18dc6bb6edb128
Adam <adkap2@gmail.com>
2021-04-27 09:16:45 -0700

    Cleaned predict_lines

--- predict_lines.py
+++ predict_lines.py
@@ -1,7 +1,5 @@
 import numpy as np
 import cv2
-# from moviepy.editor import VideoFileClip
-from IPython.display import HTML
 from keras.models import load_model
 import pickle
 import matplotlib.pyplot as plt
@@ -9,6 +7,8 @@ from PIL import Image
 import glob
 
 class Lanes():
+    """ Stores lanes values over multiple images
+    """
     def __init__(self):
         self.recent_fit = []
         self.avg_fit = []
@@ -26,8 +26,6 @@ def make_lines(image, model, lanes):
 
     # OpenCV takes images in 0-255 format while NeuralNets prefer 0-1 format
     prediction = model.predict(small_img)[0] * 255
-    # print(prediction)
-
     lanes.recent_fit.append(prediction)
     # Only using last five for average
     if len(lanes.recent_fit) > 5:
@@ -47,8 +45,8 @@ def make_lines(image, model, lanes):
 
     # Merge the lane drawing onto the original image
     combined = (image*1 + lane_image*1).astype(int)
-
     return combined # Should be combined
+
 
 def make_video():
     """ Takes images from image directory and generates
@@ -67,11 +65,13 @@ def make_video():
         out.write(img_array[i])
     out.release()
 
+
 def plot_combined(result):
     """ Plots image overlayed image"""
 
     plt.imshow(result)
     plt.show()
+
 
 def plot_side_by_side(image, lane_image):
     """ Plots predicted and original image in side by side
@@ -79,12 +79,8 @@ def plot_side_by_side(image, lane_image):
     fig, axs = plt.subplots(1,2)
     axs[0].imshow(lane_image)
     axs[1].imshow(image)
-
     plt.show()
 
-#grab last 4 characters of the file name:
-def last_4chars(x):
-    return(x[0])
 
 def load_images(path):
     """ Loads image from filepath into image_list array"""
@@ -93,7 +89,7 @@ def load_images(path):
     i = 0
     files = sorted(glob.glob(path))
     for filename in files:
-        if len(filename) == 85:
+        if len(filename) > 1:
             print(filename)
             im = Image.open(filename)
             image_list.append(im)
@@ -101,6 +97,7 @@ def load_images(path):
                 break
             i += 1
     return image_list
+
 
 def predict_unseen(path, model, lanes):
     """Generates predictions from unseen image data
@@ -117,6 +114,7 @@ def predict_unseen(path, model, lanes):
         im = Image.fromarray((combined).astype(np.uint8))
         im.save(f"continous_driving/img{i}.png")
 
+
 def make_pred_seen(model, lanes):
     """ makes prediction on already seen images """
     #img = cv2.imread(path)
@@ -128,6 +126,13 @@ def make_pred_seen(model, lanes):
     combined1 = make_lines(imgs[12], model, lanes)
     plot_combined(combined0)
     plot_combined(combined1)
+    im = np.array(imgs[8000])
+    pkl = open('full_CNN_labels.p', 'rb')
+    labels = pickle.load(pkl)
+    lab = np.array(labels[8000])
+    cv2.imwrite('example_data_image2.png', im)
+    cv2.imwrite('example_data_label2.png', lab)
+
 
 def get_model():
     """ Returns selected CNN model to generate predictions on"""
@@ -138,20 +143,19 @@ def get_model():
 
     return model
 
+
 def main():
     """ Main functions which calls all other functions in program file
     """
     model = get_model()
     print(model.summary())
-
     lanes = Lanes()
     make_pred_seen(model, lanes)
-
     # path = "/Users/adam/Desktop/galvanize/capstones/semantic_lane_detect/data/data_road/testing/image_2/*.png"
-    path = "/Users/adam/Desktop/galvanize/capstones/semantic_lane_detect/driving_dataset/*.jpg"
+    path = "/Users/adam/Desktop/galvanize/capstones/semantic_lane_detect/driving_dataset/partc/*.jpg"
 
     predict_unseen(path, model, lanes)
-    make_video()
+    # make_video()
 
 
 if __name__=="__main__":
